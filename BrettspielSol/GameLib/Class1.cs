026889a7da8e059ceecbc2cbd82d3764f88f2331
Masaru26 <jonas.hirsch@gmx.at>
2019-05-09 15:12:29 +0200

    Created Serverside Library GameLib.

---
+++ BrettspielSol/GameLib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GameLib
+{
+    public class Player
+    {
+        public static int IDCounter = 0;
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+
+        public Player(int iD, string name, string birthDate)
+        {
+            this.ID = iD;
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + this.ID + " Name: " + this.Name + " BirthDate: " + this.BirthDate;
+        }
+
+    }
+}
